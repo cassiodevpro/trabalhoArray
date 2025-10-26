31320691dc83cbdb306950dcedfeec4d87a893e5
CASSIO-DEV-PRO <cassiouitec@gmail.com>
2025-10-26 14:00:32 -0300

    Terceira tarefa

--- trabalhoArray/src/App.java
+++ trabalhoArray/src/App.java
@@ -1,26 +1,33 @@
 import atividade1.Atividade1;
 import atividade2.Atividade2;
+import atividade3.Atividade3;
+
 import java.util.Arrays;
 
 public class App {
     public static void main(String[] args) {
-        // Exemplo da Atividade1
+        //  Atividade1
         Atividade1 atividate = new Atividade1(5);
         atividate.preencher();
         atividate.mostrar();
 
-        // Array de Atividade2 dentro do main
+        // Atividade2
         Atividade2[] nomes = {
                 new Atividade2("Carlos"),
                 new Atividade2("Bruno"),
                 new Atividade2("Ana")
         };
-
         Arrays.sort(nomes, (n1, n2) -> n1.getNome().compareToIgnoreCase(n2.getNome()));
 
         System.out.println("Nomes em ordem alfabética:");
         for (Atividade2 a : nomes) {
             System.out.println(a.getNome());
         }
+
+        //Atividade 3
+        Atividade3 pares = new Atividade3(10);
+        pares.exibirPares();
+        pares.preencherPares();
+
     }
 }
