889548144703cd34074e8d67c97e9a580d783173
Cassio Moreira <cassiodevpro@gmail.com>
2025-10-26 01:55:23 +0000

    Atividade 1

---
+++ trabalhoArray/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        Atividade1 atividate = new Atividade1(30); 
+        atividate.preencher();
+        atividate.mostrar();
+    }
+}
---
+++ trabalhoArray/src/atividade1/Atividade1.java
@@ -0,0 +1,20 @@
+public class Atividade1 {
+    private int[] numeros;
+
+    public Atividade1(int tamanho) {
+        this.numeros = new int[tamanho];
+        }
+
+    public void preencher() {
+        int valor = 1;
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = valor++;
+        }
+    }
+
+    public void mostrar() {
+        for (int numero : numeros) {
+            System.out.println("numero impresso: " + numero + " e posião do numero é: " + numeros);
+        }
+    }
+}
