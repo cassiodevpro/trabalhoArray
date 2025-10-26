a9861b27d48c8529a6419b3162a172d2832a86c2
CASSIO-DEV-PRO <cassiouitec@gmail.com>
2025-10-26 00:15:40 -0300

    segunda tarefa

--- trabalhoArray/src/App.java
+++ trabalhoArray/src/App.java
@@ -1,7 +1,26 @@
+import atividade1.Atividade1;
+import atividade2.Atividade2;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) {
-        Atividade1 atividate = new Atividade1(30); 
+        // Exemplo da Atividade1
+        Atividade1 atividate = new Atividade1(5);
         atividate.preencher();
         atividate.mostrar();
+
+        // Array de Atividade2 dentro do main
+        Atividade2[] nomes = {
+                new Atividade2("Carlos"),
+                new Atividade2("Bruno"),
+                new Atividade2("Ana")
+        };
+
+        Arrays.sort(nomes, (n1, n2) -> n1.getNome().compareToIgnoreCase(n2.getNome()));
+
+        System.out.println("Nomes em ordem alfabética:");
+        for (Atividade2 a : nomes) {
+            System.out.println(a.getNome());
+        }
     }
 }
---
+++ trabalhoArray/src/atividade2/Atividade2.java
@@ -0,0 +1,16 @@
+package atividade2;
+
+public class Atividade2 {
+    private String nome;
+
+    public Atividade2(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
